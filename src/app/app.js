d9ec89d9064a56e0368c3666964581c5f70eb100
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-05-10 08:40:48 -0700

    Add React Router

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, browserHistory } from 'react-router';
 import Main from './Main'; // Our custom react component
 
 //Needed for onTouchTap
@@ -11,4 +12,8 @@ injectTapEventPlugin();
 
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}/>
+  </Router>
+  , document.getElementById('app'));
