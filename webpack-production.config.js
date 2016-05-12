85b702b48d6fb7ca017c110a669f62b5379984bf
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-05-12 03:40:46 -0700

    Fix error when compiling prod build

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -37,7 +37,7 @@ const config = {
     loaders: [
       {
         test: /\.js$/, // All .js files
-        loaders: ['babel-loader'], //react-hot is like browser sync and babel loads jsx and es6-7
+        loaders: ['babel-loader?presets[]=es2015,presets[]=stage-0,presets[]=react'], //react-hot is like browser sync and babel loads jsx and es6-7
         exclude: [nodeModulesPath],
       },
     ],
